48628bc3876d8b48bcf999823d41b779e24f5745
rzhao777 <48816828+rzhao777@users.noreply.github.com>
2026-02-22 11:55:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,7 @@ sed -i '1i src-git flrz https://github.com/flrz/openwrt-packages' feeds.conf.def
 sed -i '2i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '3i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '3i src-git golang https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang' feeds.conf.default
+#sed -i '3i src-git golang https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang' feeds.conf.default
 #sed -i '3i src-git openwrt-passwall-packages https://github.com/flrz/openwrt-passwall-packages' feeds.conf.default
 #sed -i '3i src-git passwall_packages https://github.com/flrz/passwall-packages' feeds.conf.default
 #sed -i '4i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall' feeds.conf.default
