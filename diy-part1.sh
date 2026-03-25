e4ee6e388852187a510793354bd8991ed97d46c4
god777 <48816828+rzhao777@users.noreply.github.com>
2026-03-25 11:03:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,11 +16,11 @@
 #git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 #sed -i 's/src-git packages https:\/\/github.com\/openwrt\/packages.git/src-git packages https:\/\/github.com\/immortalwrt\/packages.git/g' feeds.conf.default
 # Add a feed source
-#sed -i '1i src-git themes https://github.com/rzhao777/openwrt-themes' feeds.conf.default
+sed -i '1i src-git themes https://github.com/rzhao777/openwrt-themes' feeds.conf.default
 #sed -i '2i src-git immortalwrt_packages https://github.com/immortalwrt/packages' feeds.conf.default
-#sed -i '2i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '2i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '3i src-git golang https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang' feeds.conf.default
 #sed -i '3i src-git openwrt-passwall-packages https://github.com/flrz/openwrt-passwall-packages' feeds.conf.default
 #sed -i '3i src-git passwall_packages https://github.com/flrz/passwall-packages' feeds.conf.default
