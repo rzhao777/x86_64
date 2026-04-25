cd268cafa07b92ebf125f8ead3a901d65ab2c6e2
god777 <48816828+rzhao777@users.noreply.github.com>
2026-04-25 18:55:19 +0800

    Update update-sub.sh

--- scripts/update-sub.sh
+++ scripts/update-sub.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-SUB_URL="你的订阅地址"
+URL="你的订阅地址"
 
-curl -s $SUB_URL -o /tmp/sub.json
-
-# 假设已转换为 sing-box 格式
+curl -s $URL -o /tmp/sub.json
 cp /tmp/sub.json /etc/sing-box/config.json
 
 /etc/init.d/singbox restart
